7a3562dcb27a6f80dae6227b7a281bb567b334c1
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2024-01-18 21:18:25 +0545

    setting up canvas

--- appleCatcher/main.js
+++ appleCatcher/main.js
@@ -1,1 +1,12 @@
 import "./style.css";
+import Phaser from "phaser";
+const sizes = {
+  width,
+};
+
+const config = {
+  type: Phaser.WEBGL,
+  width: 500,
+  height: 500,
+  canvas: gameCanvas,
+};
